48f1ba1e16a3ae5f99b91b4b4c0de4066c0b495d
Malenko123 <klimentoh@hotmail.com>
2025-12-03 09:38:36 +0100

    Add files via upload

---
+++ templates/map-template.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template Name: Full Screen Weather Map
+ * Description: A full-screen template for the weather station map
+ * 
+ * This template is handled by output buffering in the template class
+ */
